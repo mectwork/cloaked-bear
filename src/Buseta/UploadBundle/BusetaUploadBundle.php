9164325d7113d895d39ce4c5fe089530bd10ad34
Luis Daniel <luisdanielsc@gmail.com>
2015-03-22 14:02:41 -0400

    Agregada la entidad UploadResources.

---
+++ src/Buseta/UploadBundle/BusetaUploadBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Buseta\UploadBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BusetaUploadBundle extends Bundle
+{
+}
---
+++ src/Buseta/UploadBundle/Entity/UploadResources.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Buseta\UploadBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * UploadResources.
+ *
+ * @ORM\Table(name="uploaded_resources")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class UploadResources
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="path", type="string", length=255, nullable=true)
+     */
+    private $path;
+
+    /**
+     * @var string
+     */
+    private $temp;
+
+    /**
+     * @var \Symfony\Component\HttpFoundation\File\UploadedFile
+     *
+     * @Assert\File(
+     *     maxSize="6000000",
+     *     mimeTypes={"image/jpeg", "image/png"},
+     *     mimeTypesMessage="Por favor suba una imagen jpg ó png"
+     * )
+     *
+     * @Assert\NotNull()
+     */
+    private $file;
+
+    /**
+     * Get id.
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set path.
+     *
+     * @param string $path
+     *
+     * @return UploadResources
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set file.
+     *
+     * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        if (isset($this->path)) {
+            $this->temp = $this->path;
+            $this->path = null;
+        } else {
+            $this->path = 'initial';
+        }
+    }
+
+    /**
+     * Get file.
+     *
+     * @return \Symfony\Component\HttpFoundation\File\UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Get absolute path.
+     *
+     * @return string
+     */
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    /**
+     * Get web path.
+     *
+     * @return string
+     */
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    /**
+     * Get upload root dir.
+     *
+     * @return string
+     */
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    /**
+     * Get upload dir.
+     *
+     * @return string
+     */
+    protected function getUploadDir()
+    {
+        return 'uploads/resources';
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    protected function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->path = $filename.'.'.$this->getFile()->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    protected function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        $this->getFile()->move($this->getUploadRootDir(), $this->path);
+
+        if (isset($this->temp)) {
+            unlink($this->getUploadRootDir().'/'.$this->temp);
+            $this->temp = null;
+        }
+
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    protected function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+}
