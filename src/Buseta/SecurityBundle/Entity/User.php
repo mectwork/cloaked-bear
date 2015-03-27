34a983b345258176c7adac35ce7869192b72fb34
Dundivet <dundivet@gmail.com>
2015-03-27 18:55:34 -0400

    Adicionando datos para Usuario

--- src/Buseta/SecurityBundle/Entity/User.php
+++ src/Buseta/SecurityBundle/Entity/User.php
@@ -17,6 +17,27 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombres", type="string", length=64)
+     */
+    private $nombres;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellidos", type="string", length=64)
+     */
+    private $apellidos;
+
+    /**
+     * @var \Buseta\UploadBundle\Entity\UploadResources
+     *
+     * @ORM\OneToOne(targetEntity="Buseta\UploadBundle\Entity\UploadResources", cascade={"persist", "remove"})
+     */
+    private $foto;
+
     public function __construct()
     {
         parent::__construct();
