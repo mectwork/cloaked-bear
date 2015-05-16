5722451da36604832616f0bc9b26ee70e805e183
Luis Daniel <luisdanielsc@gmail.com>
2015-05-16 17:19:32 -0400

    Agregada la entidad Persona de Contacto.

---
+++ src/Buseta/BodegaBundle/Controller/PersonaContactoController.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace Buseta\BodegaBundle\Controller;
+
+use Buseta\BodegaBundle\Entity\PersonaContacto;
+use Buseta\BodegaBundle\Entity\Tercero;
+use Buseta\BodegaBundle\Form\PersonaContactoType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * PersonaContacto controller.
+ *
+ * @Route("/personacontacto")
+ */
+class PersonaContactoController extends Controller
+{
+
+    /**
+     * Lists all PersonaContacto entities.
+     *
+     * @Route("/{tercero}", name="personacontacto", options={"expose": true})
+     *
+     * @Method("GET")
+     */
+    public function indexAction($tercero, Request $request)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $entities = $em->getRepository('BusetaBodegaBundle:PersonaContacto')->findByTercero($tercero);
+
+        $paginator = $this->get('knp_paginator');
+        $entities = $paginator->paginate(
+            $entities,
+            $request->query->get('page', 1),
+            10
+        );
+
+        return $this->render('BusetaBodegaBundle:PersonaContacto:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new PersonaContacto entity.
+     *
+     * @Route("/create/{tercero}", name="personacontacto_create")
+     * @ParamConverter("tercero", options={"mapping":{"tercero":"id"}})
+     *
+     * @Method("POST")
+     */
+    public function createAction(Tercero $tercero, Request $request)
+    {
+        $entity = new PersonaContacto();
+        $entity->setTercero($tercero);
+
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $trans = $this->get('translator');
+
+            $em->persist($entity);
+            $em->flush();
+
+            return new JsonResponse(array(
+                'message' => $trans->trans('messages.create.success', array(), 'BusetaBodegaBundle'),
+            ), 201);
+        }
+
+        $renderView = $this->renderView('BusetaBodegaBundle:PersonaContacto:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+
+        return new JsonResponse(array('view' => $renderView));
+    }
+
+    /**
+     * Creates a form to create a PersonaContacto entity.
+     *
+     * @param PersonaContacto $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(PersonaContacto $entity)
+    {
+        $form = $this->createForm(new PersonaContactoType(), $entity, array(
+            'action' => $this->generateUrl('personacontacto_create', array('tercero' => $entity->getTercero()->getId())),
+            'method' => 'POST',
+        ));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new PersonaContacto entity.
+     *
+     * @Route("/new/{tercero}", name="personacontacto_new", methods={"GET", "POST"}, options={"expose":true})
+     * @ParamConverter("tercero", options={"mapping":{"tercero":"id"}})
+     */
+    public function newAction(Tercero $tercero)
+    {
+        $entity = new PersonaContacto();
+        $entity->setTercero($tercero);
+
+        $form   = $this->createCreateForm($entity);
+
+        $renderView = $this->renderView('BusetaBodegaBundle:PersonaContacto:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+
+        return new JsonResponse(array('view' => $renderView));
+    }
+
+    /**
+     * Finds and displays a PersonaContacto entity.
+     *
+     * @Route("/{id}", name="personacontacto_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('BusetaBodegaBundle:PersonaContacto')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find PersonaContacto entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing PersonaContacto entity.
+     *
+     * @Route("/{id}/edit", name="personacontacto_edit", options={"expose":true})
+     *
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $entity = $em->getRepository('BusetaBodegaBundle:PersonaContacto')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find PersonaContacto entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        $renderView = $this->renderView('BusetaBodegaBundle:PersonaContacto:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+
+        return new JsonResponse(array('view' => $renderView), 202);
+    }
+
+    /**
+    * Creates a form to edit a PersonaContacto entity.
+    *
+    * @param PersonaContacto $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(PersonaContacto $entity)
+    {
+        $form = $this->createForm(new PersonaContactoType(), $entity, array(
+            'action' => $this->generateUrl('personacontacto_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        return $form;
+    }
+    /**
+     * Edits an existing PersonaContacto entity.
+     *
+     * @Route("/{id}", name="personacontacto_update")
+     * @Method("PUT")
+     * @Template("BusetaBodegaBundle:PersonaContacto:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('BusetaBodegaBundle:PersonaContacto')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find PersonaContacto entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('personacontacto_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    /**
+     * Deletes a PersonaContacto entity.
+     *
+     * @Route("/{id}", name="personacontacto_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('BusetaBodegaBundle:PersonaContacto')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find PersonaContacto entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('personacontacto'));
+    }
+
+    /**
+     * Creates a form to delete a PersonaContacto entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('personacontacto_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->getForm();
+    }
+}
---
+++ src/Buseta/BodegaBundle/Entity/PersonaContacto.php
@@ -0,0 +1,302 @@
+<?php
+
+namespace Buseta\BodegaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * PersonaContacto
+ *
+ * @ORM\Table(name="d_persona_contacto")
+ * @ORM\Entity
+ */
+class PersonaContacto
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \Buseta\BodegaBundle\Entity\Tercero
+     * @ORM\ManyToOne(targetEntity="Buseta\BodegaBundle\Entity\Tercero", inversedBy="personaContacto")
+     */
+    private $tercero;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellidos", type="string", length=255, nullable=true)
+     */
+    private $apellidos;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cargo", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $cargo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="telefono", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $telefono;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="telefono2", type="string", length=255, nullable=true)
+     */
+    private $telefono2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fax", type="string", length=255, nullable=true)
+     */
+    private $fax;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=100, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var \Buseta\BodegaBundle\Entity\Direccion
+     *
+     * @ORM\ManyToOne(targetEntity="Buseta\BodegaBundle\Entity\Direccion", inversedBy="pcontacto")
+     * @Assert\NotNull()
+     */
+    private $direccion;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return PersonaContacto
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidos
+     *
+     * @param string $apellidos
+     * @return PersonaContacto
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+    
+        return $this;
+    }
+
+    /**
+     * Get apellidos
+     *
+     * @return string 
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set cargo
+     *
+     * @param string $cargo
+     * @return PersonaContacto
+     */
+    public function setCargo($cargo)
+    {
+        $this->cargo = $cargo;
+    
+        return $this;
+    }
+
+    /**
+     * Get cargo
+     *
+     * @return string 
+     */
+    public function getCargo()
+    {
+        return $this->cargo;
+    }
+
+    /**
+     * Set telefono
+     *
+     * @param string $telefono
+     * @return PersonaContacto
+     */
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+    
+        return $this;
+    }
+
+    /**
+     * Get telefono
+     *
+     * @return string 
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * Set telefono2
+     *
+     * @param string $telefono2
+     * @return PersonaContacto
+     */
+    public function setTelefono2($telefono2)
+    {
+        $this->telefono2 = $telefono2;
+    
+        return $this;
+    }
+
+    /**
+     * Get telefono2
+     *
+     * @return string 
+     */
+    public function getTelefono2()
+    {
+        return $this->telefono2;
+    }
+
+    /**
+     * Set fax
+     *
+     * @param string $fax
+     * @return PersonaContacto
+     */
+    public function setFax($fax)
+    {
+        $this->fax = $fax;
+    
+        return $this;
+    }
+
+    /**
+     * Get fax
+     *
+     * @return string 
+     */
+    public function getFax()
+    {
+        return $this->fax;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return PersonaContacto
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set direccion
+     *
+     * @param \stdClass $direccion
+     * @return PersonaContacto
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+    
+        return $this;
+    }
+
+    /**
+     * Get direccion
+     *
+     * @return \stdClass 
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * @return Tercero
+     */
+    public function getTercero()
+    {
+        return $this->tercero;
+    }
+
+    /**
+     * @param Tercero $tercero
+     */
+    public function setTercero($tercero)
+    {
+        $this->tercero = $tercero;
+    }
+
+}
