5753689b7701a022e4c628839d9a6cd0e53c34ab
Dundivet <dundivet@gmail.com>
2015-07-10 14:29:34 -0400

    [Update] Adicionado clase Test para Autobus.

--- src/Buseta/BusesBundle/Tests/Controller/AutobusControllerTest.php
+++ src/Buseta/BusesBundle/Tests/Controller/AutobusControllerTest.php
@@ -16,6 +16,7 @@ class AutobusControllerTest extends WebTestCase
     public function setUp()
     {
         $this->client = static::createClient();
+        $this->logIn();
     }
 
     private function logIn()
@@ -31,10 +32,56 @@ class AutobusControllerTest extends WebTestCase
 
     public function testIndex()
     {
-        $this->logIn();
-
-        // Create a new entry in the database
+        // Test search/list Autobus
         $crawler = $this->client->request('GET', '/autobus/');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /autobus/");
+
+        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'new\']')->count(), 'Missing add new button');
+    }
+
+    public function testShow()
+    {
+        // Test search/list Autobus
+        $crawler = $this->client->request('GET', '/autobus/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /autobus/");
+
+        // Check data in the index view
+        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'show\']')->count(), 'Missing elements/test cases');
+        $crawler = $this->client->click($crawler->filter('a[href$=\'show\']')->eq(0)->link());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /autobus/\\d+/show");
+
+        // Check buttons in show view
+        $this->checkFunctionalityButtons($crawler, array('Editar', 'Volver', 'Eliminar'), 'Edit');
+    }
+
+    public function testEdit()
+    {
+        // Test search/list Autobus
+        $crawler = $this->client->request('GET', '/autobus/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /autobus/");
+
+        // Check data in the index view
+        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'edit\']')->count(), 'Missing elements/test cases');
+        $crawler = $this->client->click($crawler->filter('a[href$=\'edit\']')->eq(0)->link());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /autobus/\\d+/edit");
+
+        // Check buttons in edit view
+        $this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'Edit');
+    }
+
+    public function testNew()
+    {
+        // Test create Autobus
+        $crawler = $this->client->request('GET', '/autobus/new');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /autobus/new");
+
+        $this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'New');
+    }
+
+    private function checkFunctionalityButtons(\Symfony\Component\DomCrawler\Crawler $crawler, array $links, $view)
+    {
+        foreach ($links as $link) {
+            $this->assertEquals(1, $crawler->selectLink($link)->count(), sprintf('Missing "%s" link on %s view', $link, $view));
+        }
     }
 }
