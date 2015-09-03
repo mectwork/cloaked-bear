6343423d1e7acf43c1c6703786f9fafd58207f19
Dundivet <dundivet@gmail.com>
2015-09-02 22:07:18 -0400

    [New] Adicionado nomenclador Prioridad de solicitud.

---
+++ src/Buseta/NomencladorBundle/Entity/PrioridadSolicitud.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Buseta\NomencladorBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PrioridadSolicitud
+ *
+ * @ORM\Table(name="n_prioridad_solicitud")
+ * @ORM\Entity
+ */
+class PrioridadSolicitud extends BaseNomenclador
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
