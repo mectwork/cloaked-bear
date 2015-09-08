9d4166034dd932a197777ca8061e274846768e40
Dundivet <dundivet@gmail.com>
2015-09-08 04:53:27 -0400

    [Update] Actualizado menú para Módulo Taller.

---
+++ src/Buseta/TallerBundle/Menu/MenuBuilder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Buseta\TallerBundle\Menu;
+
+use HatueySoft\MenuBundle\Menu\MenuBuilder as BaseMenuBuilder;
+
+class MenuBuilder extends BaseMenuBuilder
+{
+    public function getName()
+    {
+        return 'menu_taller';
+    }
+}
