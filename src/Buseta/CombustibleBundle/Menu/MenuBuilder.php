f3e62e26fa5f6bdd6b5bff3f603abeed336f0702
Dundivet <dundivet@gmail.com>
2015-09-08 04:58:12 -0400

    [Update] Actualizado menú para Módulo Combustible.

---
+++ src/Buseta/CombustibleBundle/Menu/MenuBuilder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Buseta\CombustibleBundle\Menu;
+
+use HatueySoft\MenuBundle\Menu\MenuBuilder as BaseMenuBuilder;
+
+class MenuBuilder extends BaseMenuBuilder
+{
+    public function getName()
+    {
+        return 'menu_combustible';
+    }
+}
