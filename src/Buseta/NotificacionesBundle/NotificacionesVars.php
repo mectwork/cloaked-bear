41f56170f97e720558ab50910d610ca1ce094451
Angel Dayan <admarin2006@gmail.com>
2016-04-09 16:35:32 -0400

    [Update]Arreglos al bundle de notificaciones dejando solo dos tipos de notificaciones (interna y por correo) y agregando el asunto como parámetro

--- src/Buseta/NotificacionesBundle/NotificacionesVars.php
+++ src/Buseta/NotificacionesBundle/NotificacionesVars.php
@@ -11,11 +11,8 @@ final class NotificacionesVars {
     public static function getDefaultNotificacion()
     {
         return array(
-//            'boleta_pendiente_caja' => 'Boletas Pendientes en Caja',
-//            'casocajero_pendiente_barra' => 'Casos Cajero Pendientes en Barras',
-//            'reporte_adelantos' => 'Reporte de Adelantos',
-//            'reporte_deudas_pendientes' => 'Reporte de Deudas Pendientes en Conciliador',
-            'notificacion_generica' => 'Notificación genérica',
+            'notificacion_interna' => 'Notificación Interna',
+            'notificacion_correo' => 'Notificación por correo'
         );
     }
 } 
