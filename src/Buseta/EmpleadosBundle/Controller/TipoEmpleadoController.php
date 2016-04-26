f310ea49d980337908dca7b10acb411bfd121b48
Manuel Emilio Carpio <mectwork@gmail.com>
2016-04-26 13:46:52 -0400

    [Update] Redirección para Ver Datos de tipo de Empleados.

--- src/Buseta/EmpleadosBundle/Controller/TipoEmpleadoController.php
+++ src/Buseta/EmpleadosBundle/Controller/TipoEmpleadoController.php
@@ -193,7 +193,7 @@ class TipoEmpleadoController extends Controller
 
         if ($editForm->isValid()) {
             $em->flush();
-            return $this->redirect($this->generateUrl('tipoempleado_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('tipoempleado_show', array('id' => $id)));
         }
 
         return array(
